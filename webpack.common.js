c33d1fe0ee32a3f5d7703fd9a2b2631c1e466395
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-10-02 20:16:36 +0300

    remove html-loader

--- webpack.common.js
+++ webpack.common.js
@@ -22,10 +22,6 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
